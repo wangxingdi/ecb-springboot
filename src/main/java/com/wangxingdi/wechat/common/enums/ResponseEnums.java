780a26b4f94ce62b5298652bbb9e4b99485178a5
wangxingdi1 <wangxingdi1@jd.com>
2023-09-22 15:07:34 +0800

    feat: 密码本后台v1.0-前后端联调

--- src/main/java/com/wangxingdi/wechat/common/enums/ResponseEnums.java
+++ src/main/java/com/wangxingdi/wechat/common/enums/ResponseEnums.java
@@ -12,14 +12,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseEnums {
 
-    SUCCESS("101", "操作成功"),
-    FAIL("501", "操作失败，请稍后重试"),
+    SUCCESS("1", "操作成功"),
+    FAIL("0", "系统开了个小差，请稍后重试"),
 
-    FAIL_VALIDATION("502", "入参校验失败"),
-    FAIL_SECRET_KEY_IS_WRONG("503", "密钥错误，请重新输入"),
-
-//    FAIL_OPEN_ID_IS_NULL("502", "缺少openId信息，请重新登录微信小程序"),
-//    FAIL_LOGIN_CODE_IS_NULL("503", "缺少登录code信息，请重新登录微信小程序"),
+    FAIL_VALIDATION("500", "入参校验失败"),
+    FAIL_SECRET_KEY_IS_WRONG("501", "密钥错误，请重新输入"),
 
     ;
 
